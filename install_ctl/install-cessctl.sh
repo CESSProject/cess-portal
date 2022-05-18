290ef5cdd9599c7878040a92844f7b7d6b4ab945
jiangyizhe <1239865849@qq.com>
2022-05-18 19:47:10 +0800

    fix:Modify the one-click installation script

--- install_ctl/install-cessctl.sh
+++ install_ctl/install-cessctl.sh
@@ -11,8 +11,6 @@ cessRpcAddr=''
 faucetAddress=''
 ##Wallet private key
 idAccountPhraseOrSeed=''
-##The public key of the wallet, obtained from the following website: https://polkadot.subscan.io/tools/ss58_transform
-accountPublicKey=''
 ##Wallet public key
 walletAddress=''
 ##The storage location of the file encryption password entered when uploading the file,using an absolute address
@@ -26,11 +24,12 @@ sudo sed -i "s|boardPath : \"\"|boardPath : \"${boardPath}\"|g" ./cess_client.ya
 sudo sed -i "s|cessRpcAddr : \"\"|cessRpcAddr : \"${cessRpcAddr}\"|g" ./cess_client.yaml
 sudo sed -i "s|faucetAddress : \"\"|faucetAddress : \"${faucetAddress}\"|g" ./cess_client.yaml
 sudo sed -i "s|idAccountPhraseOrSeed : \"\"|idAccountPhraseOrSeed : \"${idAccountPhraseOrSeed}\"|g" ./cess_client.yaml
-sudo sed -i "s|accountPublicKey : \"\"|accountPublicKey : \"${accountPublicKey}\"|g" ./cess_client.yaml
 sudo sed -i "s|walletAddress : \"\"|walletAddress : \"${walletAddress}\"|g" ./cess_client.yaml
 sudo sed -i "s|keyPath : \"\"|keyPath : \"${keyPath}\"|g" ./cess_client.yaml
 sudo sed -i "s|installPath : \"\"|installPath : \"${installPath}\"|g" ./cess_client.yaml
 
+rm -rf /usr/bin/cessctl
+rm -rf /etc/cess.d/
 mkdir /etc/cess.d/
 cp ./cess_client.yaml /etc/cess.d/
 chmod 777 ./cessctl
